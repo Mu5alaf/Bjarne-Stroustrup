9557ee671f73ae51e5f09b32f610cf71a03cf4ee
Mu5alaf <madmedo73@gmail.com>
2024-12-27 12:00:10 +0200

    First app

---
+++ Hello_World/src/main.cpp
@@ -0,0 +1,15 @@
+/*
+ * # this hash is preprocessor directive
+ * include is the commend of includeing the library
+ * <name of libarary>
+ * "name of library"  looking in current dir
+    first then look in dir where standard lib are kept
+ */
+
+#include <iostream>
+
+int main()
+{
+    std:: cout << "Hello World , in C++" << "\n";
+    return 0;
+}
