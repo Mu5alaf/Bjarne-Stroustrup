88e613d2d09c1e126f2a472225c86fbe398ee293
Mu5alaf <madmedo73@gmail.com>
2024-12-27 12:26:03 +0200

    Second app

---
+++ std/src/std.cpp
@@ -0,0 +1,16 @@
+//============================================================================
+// Name        : std.cpp
+// Author      : Muhammad Khalaf
+// Version     :
+// Copyright   : Your copyright notice
+// Description : using std
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hey Writing std is pain";
+	cout << "Hello without std::";
+	return 0;
+}
