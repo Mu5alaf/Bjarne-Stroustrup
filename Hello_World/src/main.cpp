7e4e3a8782d7f35d57538906b5564a7f03aafc74
Mu5alaf <madmedo73@gmail.com>
2024-12-27 12:31:35 +0200

    Second app

--- Hello_World/src/main.cpp
+++ Hello_World/src/main.cpp
@@ -1,10 +1,15 @@
-/*
- * # this hash is preprocessor directive
- * include is the commend of includeing the library
- * <name of libarary>
- * "name of library"  looking in current dir
-    first then look in dir where standard lib are kept
- */
+//============================================================================
+// Name        : std.cpp
+// Author      : Muhammad Khalaf
+// Version     :
+// Copyright   : Your copyright notice
+// Description :
+//this hash is preprocessor directive
+// include is the commend of includeing the library
+// <name of libarary>
+// "name of library"  looking in current dir
+//  first then look in dir where standard lib are kept
+//============================================================================
 
 #include <iostream>
 
